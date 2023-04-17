3734579c782a0159e411132b7913e6678903435b
Luis Guilherme <luislebert01@outlook.com>
2023-04-17 15:19:44 -0300

    Classes criadas

---
+++ Turismo/Models/Hotel.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Turismo.Models
+{
+    internal class Hotel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public Endereco Endereco { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public decimal Valor { get; set; }
+    }
+}
--- Turismo/Models/Pacote.cs
+++ Turismo/Models/Pacote.cs
@@ -2,6 +2,11 @@
 {
     internal class Pacote
     {
-
+        public int Id { get; set; }
+        public Hotel Hotel { get; set; }
+        public Passagem Passagem { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public decimal Valor { get; set; }
+        public Cliente Cliente { get; set; }
     }
 }
