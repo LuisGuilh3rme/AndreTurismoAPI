9e2544923bd57330b0869f59e423182a07d1b0ab
Luis Guilherme <luislebert01@outlook.com>
2023-04-18 15:16:28 -0300

    Metodos iniciais do dapper criados

--- Turismo/Repositories/TurismoRepository.cs
+++ Turismo/Repositories/TurismoRepository.cs
@@ -1,80 +1,68 @@
-﻿using System.Data.SqlClient;
-using System.Globalization;
-using System.Net;
-using System.Reflection.Emit;
-using System.Runtime.InteropServices;
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using Dapper;
 using Turismo.Models;
+using static System.Formats.Asn1.AsnWriter;
 
-namespace Turismo.Services
+namespace Turismo.Repositories
 {
-    internal class TurismoService
+    internal class TurismoRepository : ITurismoRepository
     {
+        private string _connection { get; set; }
+
+        public TurismoRepository()
+        {
+            _connection = ConfigurationManager.ConnectionStrings["Turismo"].ConnectionString;
+        }
 
         public int InserirCidade(Cidade cidade)
         {
-            string insertString = "INSERT INTO Cidade (Nome) VALUES (@Nome); SELECT CAST(scope_identity() AS INT)";
-            SqlCommand insert = new SqlCommand(insertString, SQLConnection);
+            SqlConnection db = new SqlConnection(_connection);
+            db.Open();
+            db.ExecuteScalar(Cidade.INSERT, cidade);
 
-            insert.Parameters.Add(new SqlParameter("@Nome", cidade.Nome));
-
-            return Convert.ToInt32(insert.ExecuteScalar());
+            return Convert.ToInt32(db.ExecuteScalar(Cidade.INSERT, cidade));
         }
 
         public int InserirEndereco(Endereco endereco)
         {
-            string insertString = "INSERT INTO Endereco (Logradouro, Numero, Bairro, CEP, Complemento, Id_Cidade, Data_Cadastro) VALUES (@Lgdro, @Nmro, @Bairro, @CEP, @Cpmento, @IdCidade, @Data); SELECT CAST(scope_identity() AS INT)";
-            SqlCommand insert = new SqlCommand(@insertString, SQLConnection);
+            SqlConnection db = new SqlConnection(_connection);
+            db.Open();
+            db.ExecuteScalar(Endereco.INSERT, endereco);
 
-            insert.Parameters.Add(new SqlParameter("@Lgdro", endereco.Logradouro));
-            insert.Parameters.Add(new SqlParameter("@Nmro", endereco.Numero));
-            insert.Parameters.Add(new SqlParameter("@Bairro", endereco.Bairro));
-            insert.Parameters.Add(new SqlParameter("@CEP", endereco.CEP));
-            insert.Parameters.Add(new SqlParameter("@Cpmento", endereco.Complemento));
-            insert.Parameters.Add(new SqlParameter("@IdCidade", InserirCidade(endereco.Cidade)));
-            insert.Parameters.Add(new SqlParameter("@Data", endereco.DataCadastro));
-
-            return Convert.ToInt32(insert.ExecuteScalar());
+            return Convert.ToInt32(db.ExecuteScalar(Endereco.INSERT, endereco));
         }
 
         public int InserirCliente(Cliente cliente)
         {
-            string insertString = "INSERT INTO Cliente (Nome, Telefone, Id_Endereco, Data_Cadastro) VALUES (@Nome, @Telefone, @IdEndereco, @Cadastro); SELECT CAST(scope_identity() AS INT)";
-            SqlCommand insert = new SqlCommand(@insertString, SQLConnection);
+            SqlConnection db = new SqlConnection(_connection);
+            db.Open();
+            db.ExecuteScalar(Cliente.INSERT, cliente);
 
-            insert.Parameters.Add(new SqlParameter("@Nome", cliente.Nome));
-            insert.Parameters.Add(new SqlParameter("@Telefone", cliente.Telefone));
-            insert.Parameters.Add(new SqlParameter("@IdEndereco", InserirEndereco(cliente.Endereco)));
-            insert.Parameters.Add(new SqlParameter("@Cadastro", cliente.DataCadastro));
-
-            return Convert.ToInt32(insert.ExecuteScalar());
+            return Convert.ToInt32(db.ExecuteScalar(Endereco.INSERT, cliente));
         }
 
         public int InserirPassagem(Passagem passagem)
         {
-            string insertString = "INSERT INTO Passagem (Id_Origem, Id_Destino, Id_Cliente, Data, Valor) VALUES (@Org, @Dest, @IdCliente, @Data, @Valor); SELECT CAST(scope_identity() AS INT)";
-            SqlCommand insert = new SqlCommand(@insertString, SQLConnection);
+            SqlConnection db = new SqlConnection(_connection);
+            db.Open();
+            db.ExecuteScalar(Passagem.INSERT, passagem);
 
-            insert.Parameters.Add(new SqlParameter("@Org", InserirEndereco(passagem.Origem)));
-            insert.Parameters.Add(new SqlParameter("@Dest", InserirEndereco(passagem.Destino)));
-            insert.Parameters.Add(new SqlParameter("@IdCliente", InserirCliente(passagem.Cliente)));
-            insert.Parameters.Add(new SqlParameter("@Data", passagem.Data));
-            insert.Parameters.Add(new SqlParameter("@Valor", passagem.Valor));
-
-            return Convert.ToInt32(insert.ExecuteScalar());
+            return Convert.ToInt32(db.ExecuteScalar(Passagem.INSERT, passagem));
         }
 
         public int InserirHotel(Hotel hotel)
         {
-            string insertString = "INSERT INTO Hotel (Nome, Id_Endereco, Data_Cadastro, Valor) VALUES (@Nome, @IdEndereco, @Cadastro, @Valor); SELECT CAST(scope_identity() AS INT)";
-            SqlCommand insert = new SqlCommand(@insertString, SQLConnection);
+            SqlConnection db = new SqlConnection(_connection);
+            db.Open();
+            db.ExecuteScalar(Hotel.INSERT, hotel);
 
-            insert.Parameters.Add(new SqlParameter("@Nome", hotel.Nome));
-            insert.Parameters.Add(new SqlParameter("@IdEndereco", InserirEndereco(hotel.Endereco)));
-            insert.Parameters.Add(new SqlParameter("@Cadastro", hotel.DataCadastro));
-            insert.Parameters.Add(new SqlParameter("@Valor", hotel.Valor));
-
-            return Convert.ToInt32(insert.ExecuteScalar());
+            return Convert.ToInt32(db.ExecuteScalar(Hotel.INSERT, hotel));
         }
 
         public void AtualizarCampo(int id, string tabela, string campo, string atualizarString)
