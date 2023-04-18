d85ced2008293815f122e9d921795fbdf1793b45
Luis Guilherme <luislebert01@outlook.com>
2023-04-18 20:44:42 -0300

    Interface com assinaturas feitas

--- Turismo/Repositories/ITurismoRepository.cs
+++ Turismo/Repositories/ITurismoRepository.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Turismo.Models;
 
 namespace Turismo.Repositories
 {
     internal interface ITurismoRepository
     {
+        int InserirCidade(Cidade cidade);
+        int InserirEndereco(Endereco endereco);
+        int InserirCliente(Cliente cliente);
+        int InserirPassagem(Passagem passagem);
+        int InserirHotel(Hotel hotel);
+        void Inserir(Pacote pacote);
+        void AtualizarCampo(int id, string tabela, string campo, string atualizarString);
+        void RemoverPacote(int id);
+        List<Pacote> FindAll();
+        Cidade RetornarCidade(int id);
+        Endereco RetornarEndereco(int id);
+        Cliente RetornarCliente(int id);
+        Passagem RetornarPassagem(int id);
+        Hotel RetornarHotel(int id);
+
     }
 }
